377c0efed6fb117d91e136af4dc090a4ba91f6ae
Sasha Norton <sashanorton@educake.co.uk>
2026-02-11 10:00:47 +0000

    force a fresh connection for each request

--- src/Endpoints/BootstrapEndpoint.php
+++ src/Endpoints/BootstrapEndpoint.php
@@ -63,7 +63,8 @@ class BootstrapEndpoint
         return new Client([
             'headers' => [
                 'Authorization' => 'Basic ' . base64_encode($this->token . ':'),
-                'User-Agent'    => 'wonde-php-client-' . \Wonde\Client::version
+                'User-Agent'    => 'wonde-php-client-' . \Wonde\Client::version,
+                'Connection' => 'close'
             ]
         ]);
     }
